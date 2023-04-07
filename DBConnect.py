36b41b8099253082e9097147c0552df6a52564fb
hyxdev <94062878+hyxdev@users.noreply.github.com>
2023-04-07 22:45:13 +0800

    debug mode

--- DBConnect.py
+++ DBConnect.py
@@ -1,9 +1,6 @@
 import pymysql
 from flask import Flask, request, render_template, url_for
 app = Flask(__name__)
-app.debug = True
-
-
 
 @app.route("/")
 def index():
@@ -45,4 +42,4 @@ def register():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
